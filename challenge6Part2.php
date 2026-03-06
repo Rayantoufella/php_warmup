744d68583dae5bb76a71397da1ee4a8ad920819c
Rayan <rayantoufella2@gmail.com>
2026-03-06 14:02:33 +0000

    challenge 6 Part 1 is Done !

---
+++ challenge6Part2.php
@@ -0,0 +1,8 @@
+<?php 
+function calculeArea($width, $height){
+    return $width * $height;
+}
+
+$totalArea = calculeArea(7, 6); //for example
+echo "Total area is : " . $totalArea . " square units";
+
